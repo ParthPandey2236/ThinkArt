5bd944ef1c1b7b398f73affc089a32d57049ee53
Soham-2411 <soham.s2411@gmail.com>
2021-04-03 17:06:35 +0530

    Logout

--- lib/Pages/Profilepage.dart
+++ lib/Pages/Profilepage.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:think_art/authentication.dart';
 
 class Profile extends StatefulWidget {
@@ -66,43 +68,67 @@ class _ProfileState extends State<Profile> {
                           fontWeight: FontWeight.w400))
                 ])),
               )),
-          Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: EdgeInsets.only(top: height * 0.25),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.red[400],
-                        elevation: 8,
-                        shadowColor: Colors.red[200],
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(15)))),
-                    onPressed: () {},
-                    child: Container(
-                        width: 100,
-                        height: 50,
-                        child: Center(child: Text("Deposit"))),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.red[400],
-                        elevation: 8,
-                        shadowColor: Colors.red[200],
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(15)))),
-                    onPressed: () {},
-                    child: Container(
-                        width: 100,
-                        height: 50,
-                        child: Center(child: Text("Refresh"))),
-                  ),
-                ],
-              ),
+          Padding(
+            padding: EdgeInsets.only(top: height * 0.3),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.red[400],
+                          elevation: 8,
+                          shadowColor: Colors.red[200],
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)))),
+                      onPressed: () {},
+                      child: Container(
+                          width: 100,
+                          height: 50,
+                          child: Center(child: Text("Deposit"))),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.red[400],
+                          elevation: 8,
+                          shadowColor: Colors.red[200],
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)))),
+                      onPressed: () {},
+                      child: Container(
+                          width: 100,
+                          height: 50,
+                          child: Center(child: Text("Refresh"))),
+                    ),
+                  ],
+                ),
+                SizedBox(height: width * 0.08),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.red[400],
+                      elevation: 8,
+                      shadowColor: Colors.red[200],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15)))),
+                  onPressed: () async {
+                    final prefs = await SharedPreferences.getInstance();
+                    await FirebaseAuth.instance.signOut();
+                    prefs.setString('think_art_email', '');
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Authentication()));
+                  },
+                  child: Container(
+                      width: 100,
+                      height: 50,
+                      child: Center(child: Text("Logout"))),
+                ),
+              ],
             ),
           )
         ],
@@ -115,7 +141,7 @@ class CurvePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     var paint = Paint();
-    paint.color = Colors.white;
+    paint.color = Colors.white.withOpacity(0.8);
     paint.style = PaintingStyle.fill;
 
     var path = Path();
